ae392b87841d53b323aaa7db8b7929248c251549
Mohammed-Awadallah <dr.mohammed.j.awadallah@gmail.com>
2022-03-21 16:33:02 +0300

    did the lab

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # caps
 
-Practicing on event driven programming this program utilizes the hub to log events based on set intervals
+Practicing on Code Academy Parcel Service using Socket.io connection with 2 clients and a server
 
 
 ## Pull Request : 
 
 * [PR:1](https://github.com/Mohammed-Awadallah/caps/pull/1)
-
+* [PR:2](https://github.com/Mohammed-Awadallah/caps/pull/3)
 
 
  ## UML 
-  
+  ### events
   ![UML](https://cdn.discordapp.com/attachments/933696699040407562/953920912577658920/mohammed-UML.png)
+
+  ### socket.io
+  ![UML](https://i.ibb.co/jJ2nJQG/uml.png)
+
+
+  ## Output : 
+  ### socket .io
+  ![socket.io](https://i.ibb.co/fM4TnFj/terminal.png)
--- caps.js
+++ caps.js
@@ -1,6 +1,49 @@
-'use strict';
-const events = require('./events.js');
-require('./lib/vendor/vendor.js');
-require('./lib/driver/driver.js');
-// events.on('EVENT', (payload) => {
+"use strict";
+require("dotenv").config();
+const PORT = process.env.PORT || 3000;
+const hub = require("socket.io")(PORT);
+// const caps = hub.of("/caps");
+// hub.on("connection", (socket) => {
+//   console.log("CONNECTED", socket.id);
 // });
+hub.on("connection", (socket) => {
+  console.log("connected on", socket.id);
+  socket.on("pickup", (payload) => {
+    pickup(payload);
+    hub.emit('pickup' , payload);
+  });
+  socket.on("in-transit", (payload) => {
+    inTransit(payload);
+  });
+  socket.on("delivered", (payload) => {
+    delivered(payload);
+      socket.emit('delivered' , payload);
+  });
+});
+function delivered(payload) {
+  let date = new Date();
+  let deliveredEvent = {
+    event: "delivered",
+    time: date.toString(),
+    payload: payload,
+  };
+  console.log("EVENT", deliveredEvent);
+}
+function inTransit(payload) {
+  let date = new Date();
+  let inTRansitEvent = {
+    event: "in-transit",
+    time: date.toString(),
+    payload: payload,
+  };
+  console.log("EVENT", inTRansitEvent);
+}
+function pickup(payload) {
+  let date = new Date();
+  let pickupEvent = {
+    event: "pickup",
+    time: date.toString(),
+    payload: payload,
+  };
+  console.log("EVENT", pickupEvent);
+}
