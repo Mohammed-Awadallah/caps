4a38334f6159cbe99a71110fb918255f9591a607
Mohammed-Awadallah <dr.mohammed.j.awadallah@gmail.com>
2022-03-22 22:44:44 +0300

    added testing

--- __tests__/events.test.js
+++ __tests__/events.test.js
@@ -1,35 +1,109 @@
 'use strict';
-const events = require('../events')
-let data = {
-    store: 'Mcdonalds',
-    orderId: '6538989fjs-sddsfsdfdsf-428877878384d-a565-48c82be5',
-    customer: 'mohammed',
-    address: 'Tabarbour'
-}
-describe('Test events', () => {
-    let consoleSpy;
+const port = 3000;
+const io = require('socket.io')(port);
+const { faker } = require('@faker-js/faker');
 
+let socketObj ={};
+let data = {
+    store: "my store",
+    orderID: faker.datatype.uuid(),
+    customer: faker.name.findName(),
+    address: `${faker.address.city()}, ${faker.address.stateAbbr()}`,
+};
+describe('testing the connection ',()=>{
+    let consoleSpy;
     beforeAll(() => {
         consoleSpy = jest.spyOn(console, 'log').mockImplementation();
-    });
-    afterAll(() =>{
-        consoleSpy.mockRestore();
     })
     
-    it('pickup event ', async () => {
-        events.emit('pickup', data);
-        await consoleSpy();
-        expect(consoleSpy).toHaveBeenCalled();
-    });
-    it('delieverd event ', async () => {
-        events.emit('delieverd', data);
-        await consoleSpy();
-        expect(consoleSpy).toHaveBeenCalled();
-    });
-    it('in transit event ', async () => {
-        events.emit('in-transit', data);
-        await consoleSpy();
-        expect(consoleSpy).toHaveBeenCalled();
-    });
-    
+  afterAll(() =>{
+    consoleSpy.mockRestore();
 })
+
+  it('testing the client connection',async()=>{
+        io.emit('connection',socketObj);
+        await consoleSpy();
+        expect(consoleSpy).toHaveBeenCalled();
+    })
+
+})
+
+describe('testing the server (caps)', () => {
+    let consoleSpy;
+    beforeAll(() => {
+        consoleSpy = jest.spyOn(console, 'log').mockImplementation();
+    })
+
+    it('pickup emiting', async () => {
+        io.emit('pickup',data);
+        await consoleSpy();
+        expect(consoleSpy).toHaveBeenCalled();
+    })
+    it('in-transit emiting', async () => {
+        io.emit('in-transit',data);
+        await consoleSpy();
+        expect(consoleSpy).toHaveBeenCalled();
+    })
+    it('delivered emiting', async () => {
+        io.emit('delivered',data);
+        await consoleSpy();
+        expect(consoleSpy).toHaveBeenCalled();
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
